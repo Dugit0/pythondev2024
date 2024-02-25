a0011df8effb0f309185e1522fcf6ba1919ad6f3
Dugit0 <dmitry.astrakhantsev@gmail.com>
2024-02-25 12:16:55 +0300

    Add help description

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -4,27 +4,28 @@ import cowsay
 
 # cowsay [-e eye_string] [-f cowfile] [-h] [-l] [-n] [-T tongue_string] [-W column] [-bdgpstwy]
 
-parser = argparse.ArgumentParser(prog='cowsay', description='configurable speaking/thinking cow (and a bit more)')
-parser.add_argument('-e', metavar='eye_string')
-parser.add_argument('-f', metavar='cowfile')
-parser.add_argument('-l', action='store_true')
-parser.add_argument('-n', action='store_true')
-parser.add_argument('-T', metavar='tongue_string')
-parser.add_argument('-W', metavar='column', type=int)
+parser = argparse.ArgumentParser(prog='cowsay', description='Configurable speaking cow (and a bit more).')
+parser.add_argument('-e', metavar='eye_string', help="set a eye string")
+parser.add_argument('-f', metavar='cowfile', help="specifies a particular cow picture file (cowfile) to use; by default search in the list cows (see -l option), if it is not found, then open it as a file")
+parser.add_argument('-l', action='store_true', help="print list cowfiles")
+parser.add_argument('-n', action='store_true', help="use for arbitrary messages with arbitrary whitespace; if it is specified, the given message will not be word-wrapped")
+parser.add_argument('-T', metavar='tongue_string', help="set a tongue string")
+parser.add_argument('-W', metavar='column', type=int, help="specifies roughly (where the message should be wrapped); the default is equivalent to -W 40 i.e. wrap words at or before the 40th column")
 
 # -bdgpstwy
 # В оригинальном cowsay эти опции не взаимоисключающие, однако используемой библиотеке невозможно полностью имитировать особенности исходной реализации
-group = parser.add_mutually_exclusive_group()
-group.add_argument("-b", action="store_true")
-group.add_argument("-d", action="store_true")
-group.add_argument("-g", action="store_true")
-group.add_argument("-p", action="store_true")
-group.add_argument("-s", action="store_true")
-group.add_argument("-t", action="store_true")
-group.add_argument("-w", action="store_true")
-group.add_argument("-y", action="store_true")
+group = parser.add_argument_group("PRESETS", "Presets for eyes and tongue of cow")
+exclusive_group = group.add_mutually_exclusive_group()
+exclusive_group.add_argument("-b", action="store_true", help="Borg cow")
+exclusive_group.add_argument("-d", action="store_true", help="dead cow")
+exclusive_group.add_argument("-g", action="store_true", help="greedy cow")
+exclusive_group.add_argument("-p", action="store_true", help="paranoia cow")
+exclusive_group.add_argument("-s", action="store_true", help="stoned cow")
+exclusive_group.add_argument("-t", action="store_true", help="tired cow")
+exclusive_group.add_argument("-w", action="store_true", help="wired cow")
+exclusive_group.add_argument("-y", action="store_true", help="youthful cow")
 
-parser.add_argument('message', nargs='?')
+parser.add_argument('message', nargs='?', help="message that the cow says")
 
 # cowsay.cowsay()
 args = parser.parse_args()
