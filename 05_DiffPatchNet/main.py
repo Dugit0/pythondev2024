b66ce95ed8549a7a1ea593ba1f3ddc659ce2556d
Dugit0 <dmitry.astrakhantsev@gmail.com>
2024-03-18 13:29:37 +0300

    Add login, say and yield command

--- 05_DiffPatchNet/main.py
+++ 05_DiffPatchNet/main.py
@@ -15,63 +15,94 @@ clients = {}
 
 def who():
     global clients
-    return clients.keys()
+    return sorted(list(clients.keys()))
 
 def cows():
     return sorted(list(set(cowsay.list_cows()) - set(who())))
 
 async def chat(reader, writer):
-    me = "{}:{}".format(*writer.get_extra_info('peername'))
-    print(me)
-    flag_reg = False
+    global clients
+    # Unregistered user loop
     while not reader.at_eof():
         cmd = await reader.readline()
-        match cmd.decode().strip():
-            case 'who':
-                recv = ' '.join(who()) + '\n'
-                writer.write(recv.encode())
+        match shlex.split(cmd.decode().strip()):
+            case ['who']:
+                writer.write((' '.join(who()) + '\n').encode())
                 await writer.drain()
-            case 'cows':
-                recv = ' '.join(cows()) + '\n'
-                writer.write(recv.encode())
+            case ['cows']:
+                writer.write((' '.join(cows()) + '\n').encode())
                 await writer.drain()
-            case 'quit':
-                print(me, "DONE")
+            case ['login', cowname]:
+                if cowname not in cows():
+                    writer.write('Incorrect name\n'.encode())
+                    await writer.drain()
+                else:
+                    me = cowname
+                    break
+            case ['quit']:
                 writer.close()
                 await writer.wait_closed()
-                break
+                return
             case _:
-                print(cmd)
-                writer.write(cmd)
+                writer.write('Unknown command\n'.encode())
                 await writer.drain()
-    print('End', me)
+    
+    # Registered user cmd parce
+    async def parce_command(cmd):
+        global clients
+        nonlocal me, reader, writer, send, receive, flag_quit
+        match shlex.split(cmd):
+            case ['who']:
+                writer.write((' '.join(who()) + '\n').encode())
+                await writer.drain()
+            case ['cows']:
+                writer.write((' '.join(cows()) + '\n').encode())
+                await writer.drain()
+            case ['login', cowname]:
+                writer.write('You are already logged in\n'.encode())
+                await writer.drain()
+            case ['say', cowname, message]:
+                if cowname not in who():
+                    writer.write(f'{cowname} is not logged in\n'.encode())
+                    await writer.drain()
+                else:
+                    await clients[cowname].put(cowsay.cowsay(message, cow=me))
+            case ['yield', message]:
+                for other in who():
+                    if other is not me:
+                        await clients[other].put(cowsay.cowsay(message, cow=me))
+            case ['quit']:
+                send.cancel()
+                receive.cancel()
+                del clients[me]
+                writer.close()
+                await writer.wait_closed()
+                flag_quit = True
+                return
+            case _:
+                writer.write('Unknown command\n'.encode())
+                await writer.drain()
 
-
-    # read_cmd = asyncio.create_task(reader.readline())
-
-    # me = "{}:{}".format(*writer.get_extra_info('peername'))
-    # print(me)
-    # clients[me] = asyncio.Queue()
-    # send = asyncio.create_task(reader.readline())
-    # receive = asyncio.create_task(clients[me].get())
-    # while not reader.at_eof():
-    #     done, pending = await asyncio.wait([send, receive], return_when=asyncio.FIRST_COMPLETED)
-    #     for q in done:
-    #         if q is send:
-    #             send = asyncio.create_task(reader.readline())
-    #             for out in clients.values():
-    #                 if out is not clients[me]:
-    #                     await out.put(f"{me} {q.result().decode().strip()}")
-    #         elif q is receive:
-    #             receive = asyncio.create_task(clients[me].get())
-    #             writer.write(f"{q.result()}\n".encode())
-    #             await writer.drain()
-    # send.cancel()
-    # receive.cancel()
-    # print(me, "DONE")
-    # del clients[me]
-    # writer.close()
-    # await writer.wait_closed()
+    # Registered user loop
+    clients[me] = asyncio.Queue()
+    flag_quit = False
+    send = asyncio.create_task(reader.readline())
+    receive = asyncio.create_task(clients[me].get())
+    while not reader.at_eof():
+        done, pending = await asyncio.wait([send, receive], return_when=asyncio.FIRST_COMPLETED)
+        for q in done:
+            if q is send:
+                send = asyncio.create_task(reader.readline())
+                cmd = q.result().decode().strip()
+                await parce_command(cmd)
+                if flag_quit:
+                    break
+            elif q is receive:
+                receive = asyncio.create_task(clients[me].get())
+                writer.write(f"{q.result()}\n".encode())
+                await writer.drain()
+        if flag_quit:
+            break
 
 async def main():
     server = await asyncio.start_server(chat, '0.0.0.0', 1337)
